5fa80f680e7b0674ad1d971e10002ed30e7ccbe4
Kostiantyn Pasisnichenko <pasisnichenko132@gmail.com>
2024-11-18 18:22:03 +0100

    first commit on backend

---
+++ src/main/java/com/project/shop/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.project.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/home")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/gallery")
+    public String gallery() {
+        return "gallery";
+    }
+}
